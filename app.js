a5cb0ede46ff43b6b7bfc036ac232d90354db414
hushangni <shangnihu@gmail.com>
2018-07-15 20:19:04 -0400

    Finished basic coding for title page

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 const insideApp = {};
 
-const lcboURL = 'http://lcboapi.com/products';
-insideApp.lcboKey = 'MDo4NzIyYzRkYS03MDI1LTExZTgtYjRhOC00ZjdlMzc1NjY3ZmM6WWJhYUVhMnkzN0lxSUVER2ozSnYwd0RCMXZEQ3NQaGl2MEFT';
+// const lcboURL = 'http://lcboapi.com/products';
+// insideApp.lcboKey = 'MDo4NzIyYzRkYS03MDI1LTExZTgtYjRhOC00ZjdlMzc1NjY3ZmM6WWJhYUVhMnkzN0lxSUVER2ozSnYwd0RCMXZEQ3NQaGl2MEFT';
 
-insideApp.getDrinks = () => {
-    $.ajax({
-        url: lcboURL,
-        method: 'GET',
-        dataType: 'jsonp',
-        data: {
-            apikey: insideApp.lcboKey
-        }
-    }).then(function(res) {
-        console.log(res);
-    })
-}
+// insideApp.getDrinks = () => {
+//     $.ajax({
+//         url: lcboURL,
+//         method: 'GET',
+//         dataType: 'jsonp',
+//         data: {
+//             apikey: insideApp.lcboKey
+//         }
+//     }).then(function(res) {
+//         console.log(res);
+//     })
+// }
 
 const moviesURL = 'https://api.themoviedb.org/3/';
 insideApp.moviesKey = '0f074982f0e6a999d59865dff2184e86';
 
 const baseMovieImageURL = 'https://image.tmdb.org/t/p/';
 let configData = null;
-let movieToSearch = prompt();
+let movieToSearch = 'captain';
 
 insideApp.getMovies = () => {
     let url = "".concat(moviesURL, 'configuration?api_key=', insideApp.moviesKey); 
@@ -49,17 +49,19 @@ insideApp.searchMovies = (keyword) => {
         console.log("movie returned:", data.results[0]);
 
         const movie = {
-            movieTitle: data.results[0].original_title,
-            moviePosterURL: baseMovieImageURL+'w185'+ data.results[0].poster_path,
-            movieDesc: data.results[0].overview
+            title: data.results[0].original_title,
+            posterURL: baseMovieImageURL+'w185'+ data.results[0].poster_path,
+            desc: data.results[0].overview,
+            rating: data.results[0].vote_average
         };
         // const moviePosterURL = data.results[0].poster_path;
         // const movieTitle = data.results[0].original_title;
 
-        document.getElementById('movies').innerHTML = `
-        <h3>${movie.movieTitle}</h3>
-        <img src="${movie.moviePosterURL}"/>
-        <p>${movie.movieDesc}</p>
+        document.getElementById('movie').innerHTML = `
+        <h3>${movie.title}</h3>
+        <img src="${movie.posterURL}"/>
+        <p>${movie.desc}</p>
+        <p> Rating: ${movie.rating}</p>
         `;
         
     })
@@ -67,29 +69,30 @@ insideApp.searchMovies = (keyword) => {
 
 
 insideApp.init = () => {
-    insideApp.getDrinks();
+    // insideApp.getDrinks();
     insideApp.getMovies();
 }
 
 
 // document ready
 $(function() {
+
     console.log("Ready");
 
     insideApp.init();
 
-    // Konami code
-    // const pressed = [];
-    // const secretCode = 'stayin';
-    // window.addEventListener('keyup', (e) => {
-    //     pressed.push(e.key);
-    //     pressed.splice(-secretCode.length - 1, pressed.length - secretCode);
-    //     console.log(pressed);
-    //     if (pressed.join('').includes(secretCode)) {
-    //         console.log('DING DING!');
-    //         cornify_add();
-    //     }
-    // });
+    const pressed = [];
+    const secretCode = 'stayin';
+    window.addEventListener('keyup', (e) => {
+        console.log(e.key);
+        pressed.push(e.key);
+        pressed.splice(-secretCode.length - 1, pressed.length - secretCode);
+        console.log(pressed);
+        if (pressed.join('').includes(secretCode)) {
+            console.log('You activated unicorns!');
+            cornify_add();
+        }
+    });
 
 
 });
