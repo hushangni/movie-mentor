e5fb98de3aaddd14a32ca21220a5b504c0062cab
hushangni <shangnihu@gmail.com>
2018-07-22 22:25:47 -0400

    Finished project.

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ mentorApp.genres = {
     twist: ['horror', 'comedy', 'drama']
 }
 mentorApp.scrollPoints = ['#laziness', '#genre', '#time', '#ratings', '#result'];
-mentorApp.scrollPath = 'file:///Users/shangnihu/Desktop/summer-2018-js/advanced/project3/index.html?';
-
+mentorApp.scrollPath = 'file:///Users/shangnihu/Desktop/summer-2018-js/advanced/project3/index.html';
 mentorApp.keywords = {
     romance: {
         important: {
@@ -40,51 +39,52 @@ mentorApp.keywords = {
 
         unimportant: {
             modern: ['jurassic world', 'the first purge', 'kingsman', 'die hard', 'v for vendetta', 'sicario'],
-            timeless: ['kill bill', 'fugitive', 'hunger games', 'assassins', 'equilibrium']
+            timeless: ['kill bill', 'fugitive', 'hunger games', 'assassins', 'equilibrium', 'first blood']
         }
     },
     horror: {
         important: {
-            modern: ['get out', 'a quiet place', 'the cured', 'hereditary', 'are we not cats', 'the endless'],
-            timeless: []
+            modern: ['get out', 'a quiet place', 'the cured', 'hereditary', 'are we not cats', 'the endless', 'the wailing'],
+            timeless: ['28 days', 'the descent', 'insidious', 'the mist', 'battle royale', 'a tale of two sisters', 'the eye', 'mulholland drive', 'se7en', 'the usual suspects', 'the godfather', 'leon']
         },
 
         unimportant: {
-            modern: ['winchester', 'cloverfield paradox', 'the first purge'],
-            timeless: []
+            modern: ['winchester', 'cloverfield paradox', 'the first purge', 'train to busan', 'split', 'confessions', 'murder orient express'],
+            timeless: ['rec', 'the witch', 'I saw the devil', 'shutter', 'ju-on', 'cabin in the woods', 'a perfect world', 'silence of the lambs']
         }
     },
     comedy: {
         important: {
-            modern: ['get out', 'disaster artist'],
-            timeless: ['year old virgin', ]
+            modern: ['get out', 'disaster artist', 'neighbors', 'silver linings', 'the millers', 'this is the end', 'pineapple express', 'grand budapest hotel', 'big sick', 'coco', 'deadpool', 'blockers', 'big hero'],
+            timeless: ['year old virgin', 'monty python', 'borat', 'shaun of the dead', 'groundhog day', 'men in black', 'hot fuzz', 'school of rock', 'rush hour', 'dazed and confused', 'modern times']
         },
 
         unimportant: {
-            modern: [],
-            timeless: ['the room']
+            modern: ['juno', 'trainwreck', 'the heat', 'horrible bosses', 'hangover', 'the interview', 'jumanji', 'ted'],
+            timeless: ['the room', 'superbad', 'ghost busters', 'american pie', 'strangelove', 'harold and kumar white', 'white chicks', 'shanghai noon', 'austen powers', 'zoolander']
         }
     },
     drama: {
         important: {
-            modern: ['moonlight', 'room'],
-            timeless: ['slumdog millionaire', 'pulp fiction', 'memento', 'shawshank']
+            modern: ['moonlight', 'room', 'gone girl', 'black swan', 'no country for old', 'the pianist', 'grand budapest', 'isle of dogs', 'lady bird', 'selma', 'boyhood', 'dallas buyers'],
+            timeless: ['slumdog millionaire', 'pulp fiction', 'memento', 'shawshank', 'ocean\'s eleven', 'the prestige', 'schindler\'s list', 'fight club', 'million dollar baby', 'old boy']
         },
 
         unimportant: {
-            modern: [],
-            timeless: []
+            modern: ['now you see me', '12 years a slave', 'hacksaw ridge', '127 hours', 'red sparrow', 'i, tonya', 'mother!', 'killing of a sacred deer'],
+            timeless: ['amadeus', 'city lights', 'rules of the game', 'the big sleep', 'grapes of wrath', '12 angry', 'metropolis']
         }
     }
 };
 
+// find the movie based on the keyword search
 mentorApp.searchMovies = (keyword) => {
     let url = ''.concat(moviesURL, 'search/movie?api_key=', mentorApp.moviesKey, '&query=', keyword);
     fetch(url)
     .then(result=>result.json())
     .then((data)=>{
-        //process the returned data
-
+        // process the returned data
+        // create a movie object
         const movie = {
             title: data.results[0].original_title,
             posterURL: baseMovieImageURL+'w185'+ data.results[0].poster_path,
@@ -93,9 +93,9 @@ mentorApp.searchMovies = (keyword) => {
         };
 
         document.getElementById('movie').innerHTML = `
-        <h3>${movie.title}</h3>
+        <h3 class="movie-title">${movie.title}</h3>
         <img src="${movie.posterURL}"/>
-        <p>${movie.desc}</p>
+        <p class="movie-desc">${movie.desc}</p>
         <p> Rating: ${movie.rating}</p>
         `;
         
@@ -104,32 +104,28 @@ mentorApp.searchMovies = (keyword) => {
 
 // initialize
 mentorApp.init = () => {
-    // mentorApp.getMovies();
-    // grab user inputs on final submit
-    mentorApp.searchMovies('shawshank');
+    // listen for submit buttons and grab user input to search with
     $('.results-button').on('submit', (e) =>{
         e.preventDefault();
 
+        // grab appropriate data from user selection
         mentorApp.userEnergy = $('input[name=energy]:checked').val();
         $('input[name=movie-genre]:checked').val() == 'hero' ? mentorApp.userGenre = mentorApp.genres.hero[mentorApp.userEnergy] : mentorApp.userGenre = mentorApp.genres.twist[mentorApp.userEnergy];
         mentorApp.userReleaseDate = $('input[name=time-period]:checked').val();
         mentorApp.userRating = $('input[name=rating]:checked').val();
 
+        // select appropraite keyword category for movie search
         const possibleKeywords = mentorApp.keywords[mentorApp.userGenre][mentorApp.userRating][mentorApp.userReleaseDate];
+        // generate randomindex for movie keyword selection
         const randomIndex = Math.floor((Math.random() * possibleKeywords.length));
         mentorApp.searchMovies(possibleKeywords[randomIndex]);
-
-        console.log('user energy: ', mentorApp.userEnergy);
-        console.log('user genre: ', mentorApp.userGenre);
-        console.log('user release date: ', mentorApp.userReleaseDate);
-        console.log('user rating: ', mentorApp.userRating);
-
     });
 }
 
 //smooth scrolls to the given target
 mentorApp.smoothScroll = function(e) {
     for (let i = 0; i < mentorApp.scrollPoints.length; i++) {
+        console.log(e.currentTarget.action);
         if (e.currentTarget.action == `${mentorApp.scrollPath}${mentorApp.scrollPoints[i]}`) {
             document.querySelector(mentorApp.scrollPoints[i]).scrollIntoView({ 
                 behavior: 'smooth' 
@@ -138,12 +134,12 @@ mentorApp.smoothScroll = function(e) {
     }
 }
 
-// smooth scroll effect
+// listen for form submits to perform smooth scroll effect
 $('form').on('submit', (e) => {
+    console.log(e);
     e.preventDefault();
     mentorApp.smoothScroll(e);
 });
-
 
 
 // document ready
